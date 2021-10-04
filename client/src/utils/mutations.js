f89488f2dd1e0c9ba628a0f3b7f611672e2f60c9
Justin Phillips <Jukphillips1990@gmail.com>
2021-10-04 15:46:14 -0600

    created mutations helper in client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client'
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!){
+    login(email: $email, password: $password){
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!){
+    addUser(username: $username, email: $email, password: $password ){
+     token 
+     user {
+         _id
+        username
+     }   
+    }
+}`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($bookInput: BookInput!){
+    saveBook(input: $bookInput){
+        username
+        savedBooks {
+            title
+        }
+    }
+}
+
+`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookID: ID!){
+    removeBook(bookId: $bookId) {
+       username
+       saveBooks {
+           bookId
+           authors           
+           title
+           description
+           image
+           link
+       } 
+    }
+}
+
+`
+
+
