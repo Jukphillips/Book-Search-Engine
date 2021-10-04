491d74c3bc1030d73d984395b8b85d0da9622c70
Justin Phillips <Jukphillips1990@gmail.com>
2021-10-04 15:19:36 -0600

    fixed graphQl connection error

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 const { ApolloServer} = require('apollo-server-express')
-const { typeDefs, resolvers} = require('./schemas')
+const path = require('path');
+const { typeDefs, resolvers} = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+
+
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,17 +17,21 @@ app.use(express.json());
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware,
 })
 
-server.applyMiddleware({app})
+
+server.applyMiddleware({ app })
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {console.log(`🌍 Now listening on localhost:${PORT}`); console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
